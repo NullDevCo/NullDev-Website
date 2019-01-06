7261d493991f91457dccfaf3465169ed2f023c64
NullDev (Chris) <shadow@nulldev.org>
2019-01-06 05:55:45 +0100

    added routes and session stuff

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,60 +4,98 @@
 // = Copyright (c) NullDev = //
 // ========================= //
 
-let i18n = require("i18n");
+// Dependencies
+let { check, validationResult } = require("express-validator/check");
+let { matchedData } = require("express-validator/filter");
 
+
+// Utils
 let getRoutes = require("./getRoutes");
 let log = require("../utils/logger");
 let conf = require("../utils/configHandler");
 
 // Modules
 let robots = require("./modules/robots");
+let sitemap = require("./modules/sitemap");
 
-let config = conf.getConfig();
-
-let isset = function(obj){
-    return !!(obj && obj !== null && (typeof obj === "string" || typeof obj === "number" && obj !== "") || obj === 0);
-};
+let routes;
 
 module.exports = function(app){
-    app.use(function(req, res, next){
-        // log("IP " + req.ip + " requested route '" + url.parse(req.url).pathname + "'");
-
-        if (isset(req.query.locale)){
-            res.cookie(config.server.cookie_prefix + "lang", req.query.locale, { maxAge: 900000, httpOnly: true });
-            i18n.setLocale(req.query.locale);
-            res.redirect(req.originalUrl.split("?").shift());
-        }
-
-        if (isset(req.cookies[config.server.cookie_prefix])) i18n.setLocale(req.cookies[config.server.cookie_prefix]);
-
-        next();
-    });
-
     app.get("/", (req, res) => {
-        let currentLocale = i18n.getLocale(req);
         res.render("index", {
-            "currentLanguage": currentLocale,
             "routeTitle": "Home",
             "route": req.path,
-            "conf": conf
+            "conf": conf,
+            "csrfToken": req.csrfToken()
         });
+    });
+
+    app.get("/contact", (req, res) => {
+        res.render("contact", {
+            "routeTitle": "Contact",
+            "route": req.path,
+            "conf": conf,
+            "csrfToken": req.csrfToken(),
+            "data": {},
+            "errors": {}
+        });
+    });
+
+    app.post("/contact", [
+        check("message").isLength({ min: 1 }).withMessage("Message field must not be empty").trim(),
+        check("email").isEmail().withMessage("Email appears to be invalid.").trim().normalizeEmail()
+    ], (req, res) => {
+        let errors = validationResult(req);
+        if (!errors.isEmpty()){
+            res.render("contact", {
+                "routeTitle": "Contact",
+                "route": req.path,
+                "conf": conf,
+                "csrfToken": req.csrfToken(),
+                "data": req.body,
+                "errors": errors.mapped()
+            });
+        }
+
+        else {
+            let formData = matchedData(req);
+
+            req.flash("success", "Message has been delivered. I'll reply as soon as possible!");
+            res.redirect(req.originalUrl);
+        }
     });
 
     app.get("/robots.txt", (req, res) => {
         robots(req, res);
     });
 
+    app.get("/sitemap.xml", (req, res) => {
+        sitemap(req, res, routes);
+    });
+
+    // Errors
+
+    // 404
     app.get("*", (req, res) => {
-        let currentLocale = i18n.getLocale(req);
         res.render("errors/404", {
-            "currentLanguage": currentLocale,
             "routeTitle": "Not found!",
             "route": req.path,
-            "conf": conf
+            "conf": conf,
+            "csrfToken": req.csrfToken()
         });
     });
 
-    let routes = getRoutes(app);
+    // 500
+    app.use((err, req, res, next) => {
+        log.error(err.stack);
+        res.render("errors/500", {
+            "routeTitle": "Internal Server Error!",
+            "route": req.path,
+            "conf": conf,
+            "csrfToken": req.csrfToken()
+        });
+    });
+
+    routes = getRoutes(app);
     for (let i in routes) log.info("Route '" + routes[i].path + "' registered with methods '" + (routes[i].methods).join(", ") + "'");
 };
